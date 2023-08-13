d019ff826b693b33f0630629d4ab2c5879a21277
rstoyanovv <115493787+rstoyanovv@users.noreply.github.com>
2023-08-13 22:40:14 +0300

    Create deleteRow.cpp

---
+++ MatrixExercises/deleteRow.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+void deleteRow(int** matrix, int n, int m, int row) {
+    for (size_t i = 0; i < n; i++) {
+        if (i == row) {
+            for (size_t j = i; j < n; j++) {
+                for (size_t z = 0; z < m; z++) {
+                    matrix[j - 1][z] = matrix[j][z];
+                }
+            }
+        }
+    }
+}
+
+void printMatrix(int** matrix, int n, int m) {
+    for (size_t i = 0; i < n-1; i++) {
+        for (size_t j = 0; j < m; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+int main()
+{
+    int n, m, k;
+    std::cout << "Enter size of matrix: ";
+    std::cin >> n >> m;
+    if (n < 3 || n > 16 || m < 3 || m > 32) {
+        throw std::exception("Invalid size!");
+    }
+    std::cout << std::endl;
+
+    std::cout << "Enter row to delete:";
+    std::cin >> k;
+    std::cout << std::endl;
+
+    int** matrix = new int* [n];
+    for (size_t i = 0; i < n; i++) {
+        matrix[i] = new int[m];
+    }
+
+    std::cout << "Enter matrix: " << std::endl;
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    std::cout << std::endl;
+    deleteRow(matrix, n, m, k);
+    printMatrix(matrix, n, m);
+
+    for (size_t i = 0; i < n; i++) {
+        delete matrix[i];
+    }
+    delete[] matrix;
+}
