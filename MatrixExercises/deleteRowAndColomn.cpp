30dfac8068aa0628c1e2142a31475e7ce8b1447a
rstoyanovv <115493787+rstoyanovv@users.noreply.github.com>
2023-08-13 23:17:03 +0300

    Create deleteRowAndColomn.cpp

---
+++ MatrixExercises/deleteRowAndColomn.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+void deleteRow(int** matrix, int& n, int& m, int row) {
+    for (size_t i = 0; i < n; i++) {
+        if (i == row) {
+            for (size_t j = i; j < n; j++) {
+                for (size_t z = 0; z < m; z++) {
+                    matrix[j - 1][z] = matrix[j][z];
+                }
+            }
+        }
+    }
+    n--;
+}
+
+void deleteColomn(int** matrix, int& n, int& m, int col) {
+    for (size_t j = col; j < m; j++) {
+        for (size_t i = 0; i < n; i++) {
+            matrix[i][j - 1] = matrix[i][j];
+        }
+    }
+    m--;
+}
+
+void printMatrix(int** matrix, int n, int m) {
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+int main()
+{
+    int n, m, k, l;
+    std::cout << "Enter size of matrix: ";
+    std::cin >> n >> m;
+    std::cout << "Enter row to delete:";
+    std::cin >> k;
+    std::cout << "Enter colomn to delete:";
+    std::cin >> l;
+    std::cout << std::endl;
+
+    if (n < 3 || n > 16 || m < 3 || m > 32 || k < 0 || k > n || l < 0 || l > m) {
+        throw std::exception("Invalid input!");
+    }
+    std::cout << std::endl;
+      
+    int** matrix = new int* [n];
+    for (size_t i = 0; i < n; i++) {
+        matrix[i] = new int[m];
+    }
+
+    std::cout << "Enter matrix: " << std::endl;
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    std::cout << std::endl;
+    deleteRow(matrix, n, m, k);
+    printMatrix(matrix, n, m);
+    n++;
+    deleteColomn(matrix, n, m, l);
+    printMatrix(matrix, n, m);
+    m++;
+
+    for (size_t i = 0; i < n; i++) {
+        delete matrix[i];
+    }
+    delete[] matrix;
+}
